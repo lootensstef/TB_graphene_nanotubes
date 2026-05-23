76f2776cb75afad46c0eebdbee4351342bb6c2da
lootensstef <lootensstef@gmail.com>
2026-05-23 22:45:29 +0200

    added function that computes simmetry lines inside graphene_bands.py

--- graphene_bands.py
+++ graphene_bands.py
@@ -1,1 +1,16 @@
 import numpy as np
+
+def graphene_simmetrylines(N):
+    """This function constructs the vectors of points along high simmetry lines inside the graphene brillouin zone, in relative coordinates for simplicity of calculations.
+    
+    Parameters:
+        N: Number of points for each line. Affects calculated bands smoothness and calculation speeds.
+
+    Raise:
+        InputError if N<2 or not an integer: the band construction doesn't make sense for a number of points that's fractional or too low
+    """
+
+    M_Gamma=np.array([np.linspace(0.5,0,N),np.zeros(N)])
+    Gamma_K=np.array([np.linspace(0,2/3,N),np.linspace(0,1/3,N)])
+
+    return M_Gamma, Gamma_K
