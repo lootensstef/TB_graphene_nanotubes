ecf38f45c5a8fa6043bab9b2997a7a2a600720a7
lootensstef <lootensstef@gmail.com>
2026-05-25 12:57:29 +0200

    graphene_space.py/graphene_symmetrylines: changed to lowercase variable names (PEP8 standard)

--- graphene_space.py
+++ graphene_space.py
@@ -6,12 +6,15 @@ def graphene_simmetrylines(N):
     Parameters:
         N: Number of points for each line. Affects calculated bands smoothness and calculation speeds.
 
-    Raise:
+    Returns:
+    m_to_gamma, gamma_to_k, k_to_m: the segmented symmetry lines in reciprocal space, the names are self explanatory.
+    
+    Raises:
         InputError if N<2 or not an integer: the band construction doesn't make sense for a number of points that's fractional or too low
     """
 
-    M_to_Gamma=np.array([np.linspace(1/2,0,N),np.zeros(N)])
-    Gamma_to_K=np.array([np.linspace(0,2/3,N),np.linspace(0,1/3,N)])
-    K_to_M=np.array([np.linspace(2/3,1/2,N),np.linspace(1/3,1/2,N)])
+    m_to_gamma=np.array([np.linspace(1/2,0,N),np.zeros(N)])
+    gamma_to_k=np.array([np.linspace(0,2/3,N),np.linspace(0,1/3,N)])
+    k_to_m=np.array([np.linspace(2/3,1/2,N),np.linspace(1/3,1/2,N)])
 
-    return M_to_Gamma, Gamma_to_K, K_to_M
+    return m_to_gamma, gamma_to_k, k_to_m
