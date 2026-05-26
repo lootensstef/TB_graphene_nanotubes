92552eaacad57100e9ca6936a23dfd0126ffb364
lootensstef <lootensstef@gmail.com>
2026-05-26 11:24:14 +0200

    plots.py: added the file with a rough shell of what it should do, without a full implementation yet

---
+++ plots.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import simulation #separate file that runs all the calculations and outputs the energy bands
+
+
+
+
+def main():
+    #material=material_selector() to select a graphene simulation or nanotube simulation
+    #precision=input("how many points per reciprocal line?")
+    #energybands=simulate(material, precision)
+    
+    #various pyplot graphic options
+    #plt.plot(energybands)
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
