e84d9f4183e42e889b4711aed4d90377d7188ef7
lootensstef <lootensstef@gmail.com>
2026-05-27 16:38:00 +0200

    plots.py: fixed initial basic plotting code

--- plots.py
+++ plots.py
@@ -1,16 +1,19 @@
 import matplotlib.pyplot as plt
-import simulation #separate file that runs all the calculations and outputs the energy bands
-
-
+import eb_simulation #separate file that runs all the calculations and outputs the energy bands
 
 
 def main():
-    #material=material_selector() to select a graphene simulation or nanotube simulation
-    #precision=input("how many points per reciprocal line?")
-    #energybands=simulate(material, precision)
-    
+    material, nt_n, nt_m = eb_simulation.material_selector()
+    precision= eb_simulation.precision_selector()
+    k_bands, e_p_bands, e_m_bands = eb_simulation.simulate_energybands(material, nt_n, nt_m, precision)
+
     #various pyplot graphic options
-    #plt.plot(energybands)
+    plt.figure(figsize=(16,10), dpi=150)
+    
+    plt.plot(k_bands, e_p_bands, color="black", alpha=1, linewidth=3)
+    plt.plot(k_bands, e_m_bands, color="black", alpha=1, linewidth=3)
+
+    plt.show()
 
 
 
