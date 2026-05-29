eccba59b325bba3444d609eff079436f0d804533
lootensstef <lootensstef@gmail.com>
2026-05-29 18:05:09 +0200

    graphene_space.py/graphene_symmetrylines: improved docstring function description since input value error will be handled by other functions

--- graphene_space.py
+++ graphene_space.py
@@ -7,11 +7,7 @@ def graphene_symmetrylines(N):
         N: Number of points for each line. Affects calculated bands smoothness and calculation speeds.
 
     Returns:
-    m_to_gamma, gamma_to_k, k_to_m: the segmented symmetry lines in reciprocal space, the names are self explanatory.
-    
-    Raises:
-        InputError if N<2 or not an integer: the band construction doesn't make sense for a number of points that's fractional or too low
-    """
+        m_to_gamma, gamma_to_k, k_to_m: the segmented symmetry lines in reciprocal space, the names are self explanatory"""
 
     m_to_gamma=np.array([np.linspace(1/2,0,N),np.zeros(N)])
     gamma_to_k=np.array([np.linspace(0,2/3,N),np.linspace(0,1/3,N)])
