4434fdef65fb283d30001e4eeb7f6c0d54f7c8c1
lootensstef <lootensstef@gmail.com>
2026-05-29 15:26:25 +0200

    plots.py/main: improved plotting options for better graph style

--- plots.py
+++ plots.py
@@ -9,30 +9,40 @@ def main():
 
     #various pyplot graphic options
     plt.figure(figsize=(16,10), dpi=150)
-
+    plt.rcParams["font.family"] = "Times New Roman"
 
     if material=="graphene":
-        plt.plot(k_bands, e_p_bands, color="black", alpha=1, linewidth=1)
-        plt.plot(k_bands, e_m_bands, color="blue", alpha=1, linewidth=1)
+        plt.plot(k_bands, e_p_bands, color="deepskyblue", alpha=1, linewidth=1)
+        plt.plot(k_bands, e_m_bands, color="darkorange", alpha=1, linewidth=1)
+        plt.axhline(y=0,color="red",linestyle="--",linewidth=0.8,alpha=0.5)
 
-        plt.xticks([0, precision-1, 2*precision-2, 3*precision-3], ["M", r"$\Gamma$", "K", "M"])
+        plt.title("Graphene band structure along high symmetry lines", fontsize=22)
+        plt.xlabel("Wave vector", fontsize=18)
+        plt.ylabel("Energy (eV)", fontsize=18)
+        plt.xticks([0, precision-1, 2*precision-2, 3*precision-3], ["M", r"$\Gamma$", "K", "M"], fontsize=20)
+        
     
     elif material=="nanotube":
         band_number=0
+
         while band_number < e_p_bands.size:
             start_index=(band_number)*precision
             end_index=(band_number+1)*precision
 
-            plt.plot(k_bands[start_index : end_index], e_p_bands[start_index : end_index], color="black", alpha=1, linewidth=1)
-            plt.plot(k_bands[start_index : end_index], e_m_bands[start_index : end_index], color="blue", alpha=1, linewidth=1)
+            plt.plot(k_bands[start_index : end_index], e_p_bands[start_index : end_index], color="deepskyblue", alpha=1, linewidth=1)
+            plt.plot(k_bands[start_index : end_index], e_m_bands[start_index : end_index], color="darkorange", alpha=1, linewidth=1)
+            plt.axhline(y=0,color="red",linestyle="--",linewidth=0.8,alpha=0.5)
 
             band_number+=1
         
-        plt.xticks([0,k_bands[-1]], [r"$\Gamma$", "X"])
+        plt.title(f"({nt_n},{nt_m}) carbon nanotube band structure", fontsize=22)
+        plt.xlabel("Wave vector", fontsize=18)
+        plt.ylabel("Energy (eV)", fontsize=18)
+        plt.xticks([0,k_bands[-1]], [r"$\Gamma$", "X"], fontsize=20)
+
+    plt.grid(alpha=0.3, linestyle="--")
 
     plt.show()
-
-
 
 
 
