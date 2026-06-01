3c1df70dca69e00d0ba9b46678636945fc527a04
lootensstef <lootensstef@gmail.com>
2026-06-01 21:36:29 +0200

    tests/test_graphene_space.py: added test_graphene_symmetrylines_valid_output test function

--- tests/test_graphene_space.py
+++ tests/test_graphene_space.py
@@ -17,6 +17,21 @@ def test_graphene_symmetrylines_shape():
     assert gamma_to_k.shape == (2, N)
     assert k_to_m.shape == (2, N)
 
+
+def test_graphene_symmetrylines_valid_output():
+    """This function tests that each element in the generated arrays is a finite and valid value
+
+    GIVEN: graphene symmetry line generation
+    WHEN: lines are constructed
+    THEN: no NaN or infinite values exist
+    """
+
+    N = 50
+    m_to_gamma, gamma_to_k, k_to_m = graphene_space.graphene_symmetrylines(N)
+
+    for line_array in [m_to_gamma, gamma_to_k, k_to_m]:
+        assert np.all(np.isfinite(line_array))
+
 def test_graphene_symmetrylines_endpoints():
     """This function tests that the endpoints of a symmetry line are close to the expected symmetry point coordinates
 
@@ -35,17 +50,3 @@ def test_graphene_symmetrylines_endpoints():
     assert np.isclose(m_to_gamma[1, 0], 0.0)
     assert np.isclose(m_to_gamma[1, -1], 0.0)
 
-
-def test_graphene_symmetrylines_no_nan():
-    """This function tests that each element in the generated arrays is a finite and valid value
-
-    GIVEN: graphene symmetry line generation
-    WHEN: lines are constructed
-    THEN: no NaN or infinite values exist
-    """
-
-    N = 50
-    m_to_gamma, gamma_to_k, k_to_m = graphene_space.graphene_symmetrylines(N)
-
-    for line_array in [m_to_gamma, gamma_to_k, k_to_m]:
-        assert np.all(np.isfinite(line_array))
